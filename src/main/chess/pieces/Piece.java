e42a6e801e4ec443ad43525875baa519f807e28a
cgb12121 <148226446+cgb12121@users.noreply.github.com>
2024-05-11 15:18:34 +0700

    Update Piece.java

--- src/main/chess/pieces/Piece.java
+++ src/main/chess/pieces/Piece.java
@@ -4,22 +4,28 @@ import main.chess.board.Board;
 import main.chess.board.Square;
 import main.chess.game.Team;
 
+// Lớp Piece là lớp trừu tượng đại diện cho các quân cờ trong trò chơi cờ vua
 public abstract class Piece {
-    private Team team;
+    private Team team; // Đội của quân cờ
 
+    // Constructor để khởi tạo một quân cờ với đội của nó
     public Piece(Team team) {
         this.team = team;
     }
 
+    // Phương thức trả về đội của quân cờ
     public Team getTeam() {
         return team;
     }
 
+    // Phương thức để đặt đội cho quân cờ
     public void setTeam(Team team) {
         this.team = team;
     }
 
+    // Phương thức trừu tượng trả về tên file hình ảnh của quân cờ
     public abstract String getIconFileName();
 
+    // Phương thức trừu tượng kiểm tra xem quân cờ có thể di chuyển từ ô bắt đầu đến ô kết thúc không
     public abstract boolean canMove(Board board, Square start, Square end);
 }
