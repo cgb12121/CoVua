f931caeb90e855b3b07d02f8a9171a6f122a180f
cgb12121 <148226446+cgb12121@users.noreply.github.com>
2024-05-11 15:18:47 +0700

    Update Queen.java

--- src/main/chess/pieces/Queen.java
+++ src/main/chess/pieces/Queen.java
@@ -4,21 +4,26 @@ import main.chess.board.Board;
 import main.chess.board.Square;
 import main.chess.game.Team;
 
+// Lớp Queen đại diện cho quân Hậu trong trò chơi cờ vua
 public class Queen extends Piece {
-    private final Rook rook;
-    private final Bishop bishop;
+    private final Rook rook; // Biến đại diện cho quân Xe
+    private final Bishop bishop; // Biến đại diện cho quân Tượng
 
+    // Constructor để khởi tạo một quân Hậu với đội của nó
     public Queen(Team team) {
         super(team);
-        this.rook = new Rook(team);
-        this.bishop = new Bishop(team);
+        this.rook = new Rook(team); // Khởi tạo một quân Xe với đội tương ứng
+        this.bishop = new Bishop(team); // Khởi tạo một quân Tượng với đội tương ứng
     }
 
     @Override
+    // Phương thức kiểm tra xem quân Hậu có thể di chuyển từ ô bắt đầu đến ô kết thúc không
     public boolean canMove(Board board, Square start, Square end) {
+        // Quân Hậu có thể di chuyển như quân Xe hoặc quân Tượng
         return rook.canMove(board, start, end) || bishop.canMove(board, start, end);
     }
 
+    // Phương thức trả về tên file hình ảnh của quân Hậu
     @Override
     public String getIconFileName() {
         if (this.getTeam() == Team.WHITE){
