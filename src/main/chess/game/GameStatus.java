aa3f71529968d0dd1e80b6d3355760507272c47b
cgb12121 <148226446+cgb12121@users.noreply.github.com>
2024-05-11 15:16:58 +0700

    Update GameStatus.java

--- src/main/chess/game/GameStatus.java
+++ src/main/chess/game/GameStatus.java
@@ -1,8 +1,8 @@
 package main.chess.game;
 
 public enum GameStatus {
-    ACTIVE,
-    BLACK_WIN,
-    WHITE_WIN,
-    STALEMATE,
+    ACTIVE, // Trò chơi đang diễn ra
+    BLACK_WIN, // Người chơi màu đen chiến thắng
+    WHITE_WIN, // Người chơi màu trắng chiến thắng
+    STALEMATE, // Tình trạng stalemate (không ai chiến thắng)
 }
