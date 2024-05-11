a8576ba9205f42df9a6e439d643d709851271553
cgb12121 <148226446+cgb12121@users.noreply.github.com>
2024-05-11 15:17:24 +0700

    Update Team.java

--- src/main/chess/game/Team.java
+++ src/main/chess/game/Team.java
@@ -2,19 +2,20 @@ package main.chess.game;
 
 import java.awt.Color;
 
+// Enum định nghĩa các màu của các đội trong trò chơi
 public enum Team {
-    WHITE(Color.WHITE),
-    BLACK(Color.BLACK);
+    WHITE(Color.WHITE), // Đội màu trắng
+    BLACK(Color.BLACK); // Đội màu đen
 
-    private final Color color;
+    private final Color color; // Màu của đội
 
+    // Constructor để khởi tạo một đội với màu tương ứng
     Team(Color color) {
         this.color = color;
     }
 
+    // Phương thức trả về màu của đội
     public Color getColor() {
         return color;
     }
 }
-
-
