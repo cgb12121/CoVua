b5bd7e0de6fb412a518425d5ac0ef2f21ee84f85
cgb12121 <148226446+cgb12121@users.noreply.github.com>
2024-05-11 15:17:12 +0700

    Update Move.java

--- src/main/chess/game/Move.java
+++ src/main/chess/game/Move.java
@@ -2,29 +2,34 @@ package main.chess.game;
 
 import main.chess.board.Square;
 
+// Lớp Move đại diện cho một nước đi trong trò chơi cờ vua
 public class Move {
-    private Square start;
-    private Square end;
+    private Square start; // Ô bắt đầu của nước đi
+    private Square end; // Ô kết thúc của nước đi
 
+    // Constructor để khởi tạo một nước đi từ ô bắt đầu đến ô kết thúc
     public Move(Square start, Square end) {
         this.start = start;
         this.end = end;
     }
 
+    // Phương thức trả về ô bắt đầu của nước đi
     public Square getStart() {
         return start;
     }
 
+    // Phương thức để đặt ô bắt đầu của nước đi
     public void setStart(Square start) {
         this.start = start;
     }
 
+    // Phương thức trả về ô kết thúc của nước đi
     public Square getEnd() {
         return end;
     }
 
+    // Phương thức để đặt ô kết thúc của nước đi
     public void setEnd(Square end) {
         this.end = end;
     }
 }
-
